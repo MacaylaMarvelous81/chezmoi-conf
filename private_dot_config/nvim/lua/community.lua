2410d8e42f5be14eb7eded5f0c46ebc8991fafec
Jomar Milan <jomarm@jomarm.com>
2025-09-28 19:57:36 -0700

    AstroNvim v5

--- private_dot_config/nvim/lua/community.lua
+++ private_dot_config/nvim/lua/community.lua
@@ -9,6 +9,5 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.typst" },
-  { import = "astrocommunity.terminal-integration.toggleterm-manager-nvim" },
   -- import/override with your plugins folder
 }
